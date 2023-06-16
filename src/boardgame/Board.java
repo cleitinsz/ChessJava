d601b4ce2f4f6e1fabc2202ed81731c640d231cd
cleitinsz <gabriel.playhard10@gmail.com>
2023-06-15 23:35:08 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -25,7 +25,12 @@ public class Board {
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
+    }
 
-
+    public Piece piece(int row, int column){
+        return pieces[row][column];
+    }
+    public Piece piece(Position position){
+        return pieces[position.getRow()][position.getColumn()];
     }
 }
